e39f4852b814ce51173e424d36e0bd4a56f5578f
Fink <a.fink@eastbanctech.ru>
2020-01-31 19:03:44 +0700

    Add log errod with db

--- src/main/java/ru/test/service/MessageService.java
+++ src/main/java/ru/test/service/MessageService.java
@@ -1,12 +1,15 @@
 package ru.test.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.test.model.Message;
 import ru.test.repository.MessageRepository;
 
+import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class MessageService {
@@ -14,6 +17,11 @@ public class MessageService {
     private final MessageRepository messageRepository;
 
     public Iterable<Message> saveAll(List<Message> message) {
-        return messageRepository.saveAll(message);
+        try {
+            return messageRepository.saveAll(message);
+        } catch (Exception e) {
+            // TODO(a.fink): 29.01.2020  
+        }
+        return Collections.emptyList();
     }
 }
